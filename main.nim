b4029941b46a8adc486aa990b63f689e87b26b66
andy <andy@ashall.co>
2018-07-16 13:20:45 +0100

    added help

--- main.nim
+++ main.nim
@@ -12,14 +12,21 @@ var today = ""
 
 var done = false
 
+echo "What did you do today? (Type -h for help)"
+
 while done == false:
-  var nextLine = f.readLine()
-  if nextLine == ":q":
+  var nextLine = f.readLine() & "\n"
+  if nextLine == "-h\n":
+    echo "Enter what you did today separated by returns"
+    echo "Use -s in a new line to save your lines"
+    echo "Use -q in a new line to quit without saving"
+    nextLine = ""
+  if nextLine == "-q\n":
     done = true
-  elif nextLine == ":s":
+  elif nextLine == "-s\n":
     let content = format(now(), "ddd d/MM/yyyy hh:mm") & "\n" & today & "\n" & file
     writeFile("did.txt", content)
     echo readFile("did.txt")
     done = true
   else:
-    today &= nextLine & "\n"
+    today &= nextLine
